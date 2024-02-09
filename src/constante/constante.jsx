de84d77017deefa6a8b0ce103af201fbf80b0029
Andriniaina Fabien Rakotomanana <fabrakoto0987@gmail.com>
2024-02-09 23:40:03 +0300

    modify api_url

--- src/constante/constante.jsx
+++ src/constante/constante.jsx
@@ -1,3 +1,3 @@
 export const API_URL4 = "http://localhost:8080";
 export const API_URL2 = "https://sweltering-slope-production.up.railway.app/api";
-export const API_URL = "https://backoffice-voiture-production-e819.up.railway.app"
+export const API_URL = "https://backoffice-voiture-production.up.railway.app"
